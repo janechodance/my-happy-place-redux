4995b0948b3dc1c545f3eb5d1c2540082ce78696
Jane <janechodance@gmail.com>
2021-12-20 21:30:03 -0500

    signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+    def create
+        user = User.create!(user_params)
+        session[:user_id] = user.id
+        render json: user, status: :created
+    end
     def index
         users = User.all
         render json: users, include: :vendors, status: :ok
@@ -7,5 +12,10 @@ class UsersController < ApplicationController
     def show
         user = User.find(params[:id])
         render json: user, include: :vendor, status: :ok
+    end
+    private
+
+    def user_params
+      params.permit(:username, :password, :name, :email, :phone, :address, :DOB, :is_vendor)
     end
 end
